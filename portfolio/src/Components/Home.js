bc6300e4775a4e6e82b6499df91f369a3730cfbe
Muhamed Sufail <djsufail@gmail.com>
2020-10-25 23:55:09 +0530

    Background image not working

--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './Home.css'
 function Home() {
     return (
-        <div>
-            Home Component
+        <div className="home">
+            
         </div>
     )
 }
