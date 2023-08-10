c45769bd37e4f3ec7b0e59822789d5e8b5771eed
cod3kid <djsufail@gmail.com>
2023-08-10 16:59:09 +0530

    Testing onUserAdd function

--- react-testing-library-jest/starter/src/UserForm.test.js
+++ react-testing-library-jest/starter/src/UserForm.test.js
@@ -11,3 +11,29 @@ test("shows two input and a button", () => {
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
 });
+
+test("click on add button and calls onUserAdd function", () => {
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+
+  render(<UserForm onUserAdd={callback} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+
+  user.click(nameInput);
+  user.keyboard("John Wick");
+
+  user.click(emailInput);
+  user.keyboard("johnwick@gmail.com");
+
+  user.click(button);
+
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({
+    name: "John Wick",
+    email: "johnwick@gmail.com",
+  });
+});
