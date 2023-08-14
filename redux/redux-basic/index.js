9ee64e6b716fd046afc7e97900ca88b7c2bc2504
cod3kid <djsufail@gmail.com>
2023-08-14 20:05:10 +0530

    Redux basic using createStore method

---
+++ redux/redux-basic/index.js
@@ -0,0 +1,31 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+const orderCake = () => {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+};
+
+initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+// getState method is used to get the current state of the application
+console.log("Initial State", store.getState());
