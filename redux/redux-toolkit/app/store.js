5193b999833d8724d16ff81044655a331bfeea16
cod3kid <djsufail@gmail.com>
2023-08-21 02:26:51 +0530

    Redux Toolkit: Configured Store

---
+++ redux/redux-toolkit/app/store.js
@@ -0,0 +1,8 @@
+const configureStore = require("@reduxjs/toolkit").configureStore;
+const cakeReducer = require("../features/cake/cakeSlice");
+
+const store = configureStore({
+  reducer: cakeReducer,
+});
+
+module.exports = store;
