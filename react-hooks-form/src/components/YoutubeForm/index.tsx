6c1111d6d5c44f8cd60209acb87a0755c528c5b6
cod3kid <djsufail@gmail.com>
2023-11-15 17:13:07 +0530

    React Hook Form: Async validation

--- react-hooks-form/src/components/YoutubeForm/index.tsx
+++ react-hooks-form/src/components/YoutubeForm/index.tsx
@@ -172,6 +172,15 @@ function YoutubeForm() {
                   return "Enter an allowed domain name";
                 }
               },
+              emailAvailable: async (fieldValue) => {
+                const response = await fetch(
+                  `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
+                );
+
+                const data = await response.json();
+
+                return data.length === 0 || "Email is not available";
+              },
             },
           })}
         />
