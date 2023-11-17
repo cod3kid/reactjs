b2791aab30a04c6b3f4aa3d18f415328beef48e9
cod3kid <djsufail@gmail.com>
2023-11-17 10:52:34 +0530

    React Hook Form: Yup Validation

--- react-hooks-form/src/App.tsx
+++ react-hooks-form/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import YoutubeForm from "./components/YoutubeForm";
+import YupYoutubeForm from "./components/YupYoutubeForm";
 
 function App() {
   return (
     <div className="App">
-      <YoutubeForm />
+      {/* <YoutubeForm /> */}
+      <YupYoutubeForm />
     </div>
   );
 }
