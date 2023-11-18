21a55c7a5ab7ed649dd210c3ba8f1983cb9f832e
cod3kid <djsufail@gmail.com>
2023-11-18 11:22:42 +0530

    React Hook Form: Zod Validation

--- react-hooks-form/src/App.tsx
+++ react-hooks-form/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import YoutubeForm from "./components/YoutubeForm";
 import YupYoutubeForm from "./components/YupYoutubeForm";
+import ZodYoutubeForm from "./components/ZodYoutubeForm";
 
 function App() {
   return (
     <div className="App">
       {/* <YoutubeForm /> */}
-      <YupYoutubeForm />
+      {/* <YupYoutubeForm /> */}
+      <ZodYoutubeForm />
     </div>
   );
 }
